9517b54568f8de4f68e81854a73ae0adf1209d17
151250010 <151250010@smail.nju.edu.cn>
2017-06-09 12:27:54 +0800

    修改浏览记录时间，反转浏览记录

--- js/SingleStock.js
+++ js/SingleStock.js
@@ -173,12 +173,29 @@ var vm = new Vue({
             var seperator2 = ":";
             var month = date.getMonth() + 1;
             var strDate = date.getDate();
+
+            var hour = date.getHours();
+            var minute = date.getMinutes();
+            var second = date.getSeconds();
+
             if (month >= 1 && month <= 9) {
                 month = "0" + month;
             }
             if (strDate >= 0 && strDate <= 9) {
                 strDate = "0" + strDate;
             }
+            if (hour >= 0 && hour <= 9) {
+                hour = "0" + hour;
+            }
+
+            if (minute >= 0 && minute <= 9) {
+                minute = "0" + minute;
+            }
+
+            if (second >= 0 && second <= 9) {
+                second = "0" + second;
+            }
+
             var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate
                 + " " + date.getHours() + seperator2 + date.getMinutes()
                 + seperator2 + date.getSeconds();
