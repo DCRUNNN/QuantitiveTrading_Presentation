4175916f1cdae785fabebd41612017dc7176fbbb
151250031 <151250031@smail.nju.edu.cn>
2017-06-15 03:51:37 +0800

    更新了一些链接

--- js/SearchStock.js
+++ js/SearchStock.js
@@ -73,7 +73,6 @@ var vm = new Vue({
 
         search: function () {
             var code = document.getElementById("input").value.split("-")[1];
-           alert(code);
             if(code==null||code==""){
                show1();
             } else if(!isChinese(code)&&!isNum(code)){
