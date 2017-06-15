a42ac2692fe31de346a98ca9202c57207f3df24a
151250031 <151250031@smail.nju.edu.cn>
2017-06-15 10:09:23 +0800

    将要实现删除自定义策略功能，注意如果删除个人策略的时候该策略也在策略广场中，那么也要在策略广场中将该策略删除，若策略广场中无该策略，即用户没有分享到策略广场，那么就不需要删除该策略

--- js/MyStrategy.js
+++ js/MyStrategy.js
@@ -8,7 +8,8 @@ var vm = new Vue({
 
        ],
         strategyName:"",
-        phoneNumber:""
+        phoneNumber:"",
+        strategyName_delete:""
     },
     methods:{
         getCookieValue:function (cname) {
@@ -28,11 +29,14 @@ var vm = new Vue({
 
         shareStrategy:function() {
             const self=this;
+            if(self.strategyName=="") {
+                alert("请输入要分享的策略名称");
+            }
+
             this.$http.post("http://localhost:8080/strategysquare/square",{
                 phoneNumber:self.getCookieValue("phoneNumber"),
                 strategyName:self.strategyName,
             }).then(function (response) {
-                console.log(response);
                 var check=response.data.errorCode;
                 // if(check==50000001) {
                 //     alert("该策略名称已经存在了哟！");
@@ -43,6 +47,50 @@ var vm = new Vue({
                 hide();
             }).catch(function(error){
                 alert("很抱歉，分享策略时出现了错误！")
+            })
+        },
+
+        deleteStrategy:function() {
+            const self=this;
+            if(self.strategyName_delete=="") {
+                alert("请输入要删除的策略名称");
+                return;
+            }
+
+            var check=1;
+            alert(666);
+            this.$http.post("http://localhost:8080/strategy/deleteStrategy",{
+                phoneNumber:self.getCookieValue("phoneNumber"),
+                strategyName:self.strategyName_delete,
+            }).then(function (response) {
+                check=response.data.errorCode;
+                // if(check==50000001) {
+                //     alert("该策略名称已经存在了哟！");
+                // }else
+                alert(strategyName);
+                if(check!=0) {
+                    alert("很抱歉，删除策略时出现了错误！");
+                }
+            }).catch(function(error){
+                alert("很抱歉，删除策略时出现了错误！")
+            })
+
+            this.$http.post("http://localhost:8080/strategysquare/deleteStrategy",{
+                phoneNumber:self.getCookieValue("phoneNumber"),
+                strategyName:self.strategyName_delete,
+            }).then(function (response) {
+                check=response.data.errorCode;
+                // if(check==50000001) {
+                //     alert("该策略名称已经存在了哟！");
+                // }else
+                if(check==0) {
+                    alert("删除策略成功");
+                    hide2();
+                }else{
+                    alert("很抱歉，删除策略时出现了错误！");
+                }
+            }).catch(function(error){
+                alert("很抱歉，删除策略时出现了错误！")
             })
         }
     },
@@ -59,6 +107,7 @@ var vm = new Vue({
             document.getElementById("login1").innerHTML = "已登录";
             document.getElementById("login1").href = "#";
         }
+
 
         const self = this;
         this.$http.get("http://localhost:8080/strategy/"+phoneNumber).then(function (response) {
@@ -80,4 +129,16 @@ function save(){
 function hide() {
     document.getElementById("hidebg").style.display="none";
     document.getElementById("saveBody").style.display="none";
+}
+
+function myDelete(){
+    var hideobj=document.getElementById("hidebg1");
+    hidebg1.style.display="block";  //显示隐藏层
+    hidebg1.style.height=document.body.clientHeight+"px";  //设置隐藏层的高度为当前页面高度
+    document.getElementById("deleteBody").style.display="block";  //显示弹出层
+}
+
+function hide2() {
+    document.getElementById("hidebg1").style.display="none";
+    document.getElementById("deleteBody").style.display="none";
 }
