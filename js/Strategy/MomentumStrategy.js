719cfe3d4367119c1804734f5ee9063f1e56e7e5
151250021 <pc@172.26.212.12>
2017-06-15 14:28:00 +0800

    最后的修改与优化

--- js/Strategy/MomentumStrategy.js
+++ js/Strategy/MomentumStrategy.js
@@ -67,165 +67,166 @@ var vm = new Vue({
             for(var i=0;i<this.chosens.length;i++){
                 this.stockPool.push(this.chosens[i].stockCode);
             }
-            this.$http.get("http://localhost:8080/exhibition/momentumStrategy",{
-                params:{
-                    stockPool:this.stockPool,
-                   dateRange:document.getElementById("reservation").value,
-                    dayNumFormative:this.createDays,
-                    dayNumHolding:this.holdingDays
-                }
-            }).then(function (response) {
+            if((this.createDays =="")||(this.holdingDays =="")){
+                show();
+            }else {
+                this.$http.get("http://localhost:8080/exhibition/momentumStrategy", {
+                    params: {
+                        stockPool: this.stockPool,
+                        dateRange: document.getElementById("reservation").value,
+                        dayNumFormative: this.createDays,
+                        dayNumHolding: this.holdingDays
+                    }
+                }).then(function (response) {
 
-                console.log(response.data.data.winRates);
-                console.log(response.data.data.rateNums);
+                    console.log(response.data.data.winRates);
+                    console.log(response.data.data.rateNums);
 
-                this.MomentumDate = response.data.data.dateList;
-                this.MomentumFieldRate = response.data.data.yieldRates;
-                this.MomentumPrimaryDate = response.data.data.primaryRates;
-                this.MomentumWinRates = response.data.data.winRates;
-                this.MomentumRateNums = response.data.data.rateNums;
-                this.yearProfit = response.data.data.yearYield;
-                this.primaryYearProfit = response.data.data.primaryYearYield;
-                this.alafa = response.data.data.alpha;
-                this.beita = response.data.data.beta;
-                this.sharp = response.data.data.shapeRatio;
-                this.maxBack = response.data.data.maxDrawnDown;
+                    this.MomentumDate = response.data.data.dateList;
+                    this.MomentumFieldRate = response.data.data.yieldRates;
+                    this.MomentumPrimaryDate = response.data.data.primaryRates;
+                    this.MomentumWinRates = response.data.data.winRates;
+                    this.MomentumRateNums = response.data.data.rateNums;
+                    this.yearProfit = response.data.data.yearYield;
+                    this.primaryYearProfit = response.data.data.primaryYearYield;
+                    this.alafa = response.data.data.alpha;
+                    this.beita = response.data.data.beta;
+                    this.sharp = response.data.data.shapeRatio;
+                    this.maxBack = response.data.data.maxDrawnDown;
 
-                var option1 = {
-                    title : {
-                        text:'柱状显示数据'
+                    var option1 = {
+                        title: {
+                            text: '柱状显示数据'
 
-                    },
-                    tooltip : {
-                        trigger: 'axis'
-                    },             legend: {
-                        data:['基准收益','策略收益']
-                    },
-                    toolbox: {
-                        show : true,
-                        feature : {
-                            mark : {show: true},
-                            dataView : {show: true, readOnly: false},
-                            magicType : {show: true, type: ['line', 'bar']},
-                            restore : {show: true},
-                            saveAsImage : {show: true}
-                        }
-                    },
-                    calculable : true,
-                    xAxis : [
-                        {
-                            type : 'category',
-                            boundaryGap : false,
-                            data : this.MomentumDate
-                        }
-                    ],
-                    yAxis : [
-                        {
-                            type : 'value',
-                            axisLabel : {
-                                formatter: '{value} %'
-                            }
-                        }
-                    ],
-                    series : [
-                        {
-                            name:'基准收益',
-                            type:'line',
-                            data:this.MomentumPrimaryDate,
-                            markPoint : {
-                                data : [
-                                    {type : 'max', name: '最大值'},
-                                    {type : 'min', name: '最小值'}
-                                ]
-                            },
-                            markLine : {
-                                data : [
-                                    {type : 'average', name: '平均值'}
-                                ]
+                        },
+                        tooltip: {
+                            trigger: 'axis'
+                        }, legend: {
+                            data: ['基准收益', '策略收益']
+                        },
+                        toolbox: {
+                            show: true,
+                            feature: {
+                                mark: {show: true},
+                                dataView: {show: true, readOnly: false},
+                                magicType: {show: true, type: ['line', 'bar']},
+                                restore: {show: true},
+                                saveAsImage: {show: true}
                             }
                         },
-                        {
-                            name:'策略收益',
-                            type:'line',
-                            data:this.MomentumFieldRate,
-                            markPoint : {
-                                data : [
-                                    {type : 'max', name: '最大值'},
-                                    {type : 'min', name: '最小值'}
-                                ]
-                            },
-                            markLine : {
-                                data : [
-                                    {type : 'average', name : '平均值'}
-                                ]
+                        calculable: true,
+                        xAxis: [
+                            {
+                                type: 'category',
+                                boundaryGap: false,
+                                data: this.MomentumDate
                             }
-                        }
-                    ]
-                };
-                mychart.hideLoading();
-                mychart.setOption(option1);
-
-
-                var option2 = {
-                    title : {
-
-                    },
-                    tooltip : {
-                        trigger: 'axis'
-                    },
-                    legend: {
-                        data:['频数']
-                    },
-                    toolbox: {
-                        show : true,
-                        feature : {
-                            mark : {show: true},
-                            dataView : {show: true, readOnly: false},
-                            magicType : {show: true, type: ['bar']},
-                            restore : {show: true},
-                            saveAsImage : {show: true}
-                        }
-                    },
-                    calculable : true,
-                    xAxis : [
-                        {
-                            type : 'category',
-                            axisLabel : {
-                                formatter: '{value}'
-                            },
-                            data:["-9以下","-9~-8","-8~-7","-7~-6","-6~-5","-5~-4","-4~-3","-3~-2","-2~-1","-1~0","0~1","1~2","2~3","3~4","4~5","5~6","6~7","7~8","8~9","9以上"]
-                        }
-                    ],
-                    yAxis : [
-                        {
-                            type : 'value'
-                        }
-                    ],
-                    series : [
-                        {
-                            name:'频数',
-                            type:'bar',
-                            data:this.MomentumRateNums,
-                            markPoint : {
-                                data : [
-                                    {type : 'max', name: '最大值'},
-                                    {type : 'min', name: '最小值'}
-                                ]
-                            },
-                            markLine : {
-                                data : [
-                                    {type : 'average', name: '平均值'}
-                                ]
+                        ],
+                        yAxis: [
+                            {
+                                type: 'value',
+                                axisLabel: {
+                                    formatter: '{value} %'
+                                }
                             }
-                        }
-                    ]
-                };
-                barchart.hideLoading();
-                barchart.setOption(option2);
-            }).catch(function (error) {
-                alert("发生了未知的错误！")
-            });
+                        ],
+                        series: [
+                            {
+                                name: '基准收益',
+                                type: 'line',
+                                data: this.MomentumPrimaryDate,
+                                markPoint: {
+                                    data: [
+                                        {type: 'max', name: '最大值'},
+                                        {type: 'min', name: '最小值'}
+                                    ]
+                                },
+                                markLine: {
+                                    data: [
+                                        {type: 'average', name: '平均值'}
+                                    ]
+                                }
+                            },
+                            {
+                                name: '策略收益',
+                                type: 'line',
+                                data: this.MomentumFieldRate,
+                                markPoint: {
+                                    data: [
+                                        {type: 'max', name: '最大值'},
+                                        {type: 'min', name: '最小值'}
+                                    ]
+                                },
+                                markLine: {
+                                    data: [
+                                        {type: 'average', name: '平均值'}
+                                    ]
+                                }
+                            }
+                        ]
+                    };
+                    mychart.hideLoading();
+                    mychart.setOption(option1);
 
+
+                    var option2 = {
+                        title: {},
+                        tooltip: {
+                            trigger: 'axis'
+                        },
+                        legend: {
+                            data: ['频数']
+                        },
+                        toolbox: {
+                            show: true,
+                            feature: {
+                                mark: {show: true},
+                                dataView: {show: true, readOnly: false},
+                                magicType: {show: true, type: ['bar']},
+                                restore: {show: true},
+                                saveAsImage: {show: true}
+                            }
+                        },
+                        calculable: true,
+                        xAxis: [
+                            {
+                                type: 'category',
+                                axisLabel: {
+                                    formatter: '{value}'
+                                },
+                                data: ["-9以下", "-9~-8", "-8~-7", "-7~-6", "-6~-5", "-5~-4", "-4~-3", "-3~-2", "-2~-1", "-1~0", "0~1", "1~2", "2~3", "3~4", "4~5", "5~6", "6~7", "7~8", "8~9", "9以上"]
+                            }
+                        ],
+                        yAxis: [
+                            {
+                                type: 'value'
+                            }
+                        ],
+                        series: [
+                            {
+                                name: '频数',
+                                type: 'bar',
+                                data: this.MomentumRateNums,
+                                markPoint: {
+                                    data: [
+                                        {type: 'max', name: '最大值'},
+                                        {type: 'min', name: '最小值'}
+                                    ]
+                                },
+                                markLine: {
+                                    data: [
+                                        {type: 'average', name: '平均值'}
+                                    ]
+                                }
+                            }
+                        ]
+                    };
+                    barchart.hideLoading();
+                    barchart.setOption(option2);
+                }).catch(function (error) {
+                    alert("发生了未知的错误！")
+                });
+            }
         },
 
         add:function (code,name,sector) {
